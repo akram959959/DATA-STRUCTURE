998b3a23dacee71ed5b17d87b43ec0f962d31d4e
akram959959 <96990281+akram959959@users.noreply.github.com>
2022-01-17 14:25:59 +0530

    Time: 3 ms (86.52%), Space: 39.4 MB (70.25%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -16,17 +16,3 @@ class Solution {
     }
 }
 
-/*
-if(ransomNote.length() > magazine.length())
-            return false;
-        int[] arr = new int[26];
-        for(int i = 0; i < magazine.length(); i++){
-            arr[magazine.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i < ransomNote.length(); i++){
-            arr[ransomNote.charAt(i) - 'a']--;
-            if(arr[ransomNote.charAt(i) - 'a'] < 0)
-                return false;
-        }
-        return true;
-        */
