b5941cab91d14c2e8e527bdd7ed66cf83dc4d7b3
akram959959 <96990281+akram959959@users.noreply.github.com>
2022-10-03 14:15:16 +0530

    Time: 4171 ms (5.01%), Space: 14.5 MB (78.79%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,18 +1,15 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        #bottom up
-        self.memo = {}
-        return self.helper(n, target, k)%(10**9+7)
-    
-    def helper(self, dice_n, target, k):
-        # no need to conside target less than zero cause in next for function we set target must bigger than or equal 0
-        if dice_n == 0:
-            return 0 if target > 0 else 1
-        if (dice_n, target) in self.memo:
-            return self.memo[(dice_n, target)]
-        ways = 0
-        # find the next possible candidate(base number)
-        for base in range(max(0, target - k), target):
-            ways += self.helper(dice_n - 1, base, k)
-        self.memo[(dice_n, target)] = ways
-        return ways
+        a = [[ 0 for i in range(target+1)] for j in range(n+1)]
+        a[0][0] = 1;
+        mod = 10**9 + 7
+        for i in range(1,n+1):
+            for j in range(1,target+1):
+                f=1
+                while f<= min(j,k):
+                    a[i][j] = (a[i][j] + a[i-1][j-f]) %mod
+                    f+=1
+        return a[n][target]%mod
+                
+        
+     
